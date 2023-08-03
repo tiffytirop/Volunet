c58b1518613d56555d16c53766906109b880bcc2
tiffytirop <tiffany.tirop@strathmore.edu>
2023-08-03 19:46:25 +0300

    db

--- Views/db_connection.php
+++ Views/db_connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost"; 
 $username = "root"; 
 $password = ""; 
-$database = "volunet"; 
+$database = "users"; 
 
 // Create a connection
 $conn = new mysqli($servername, $username, $password, $database);
@@ -11,4 +11,4 @@ $conn = new mysqli($servername, $username, $password, $database);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-?>
+
