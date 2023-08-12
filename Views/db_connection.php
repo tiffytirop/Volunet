406cd77b520cc1fba2f142b665b4222cc2f0c748
tiffytirop <tiffany.tirop@strathmore.edu>
2023-08-12 09:39:11 +0300

    signup

--- Views/db_connection.php
+++ Views/db_connection.php
@@ -5,10 +5,11 @@ $password = "";
 $database = "volunet"; 
 
 // Create a connection
-$conn = new mysqli($servername, $username, $password, $database);
+$conn = new mysql($servername, $username, $password, $database);
 
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
